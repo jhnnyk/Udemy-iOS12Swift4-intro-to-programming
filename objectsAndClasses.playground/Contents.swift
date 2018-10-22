8b641e30ac9b2b14e0b1db38e19510af89169e98
John Knotts <jhnnyk@gmail.com>
2018-10-22 16:54:31 -0600

    objects and classes

---
+++ objectsAndClasses.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class Vehicle {
+    var tires = 4
+    var headlights = 2
+    var horsepower = 468
+    var model = ""
+    
+    func drive() {
+        // accelerate the vehicle
+    }
+    
+    func brake() {
+        // brake the vehicle
+    }
+}
+
+let bmw = Vehicle()
+bmw.model = "328i"
+
+let ford = Vehicle()
+ford.model = "F150"
+ford.brake()
+
+func passByReference(vehicle: Vehicle) {
+    vehicle.model = "Cheese"
+}
+
+print(ford.model)
+
+passByReference(vehicle: ford) // Pass by reference
+
+print(ford.model)
